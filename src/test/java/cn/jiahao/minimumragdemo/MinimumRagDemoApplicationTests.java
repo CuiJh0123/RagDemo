4a2c4b13aae29cfd4b09c0b9facdc80d8d61c4b4
Jiahao <907108838@qq.com>
2026-03-25 16:31:49 +0800

    init

---
+++ src/test/java/cn/jiahao/minimumragdemo/MinimumRagDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.jiahao.minimumragdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimumRagDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
